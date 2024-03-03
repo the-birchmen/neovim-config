2e2c46b26eb4fd080bb1461af13312711fb9e26e
Dustin Morse <dustymorse@gmail.com>
2024-03-03 11:18:41 -0500

    more config and some new plugins

---
+++ lua/custom/plugins/alpha.lua
@@ -0,0 +1,16 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  {
+    'goolord/alpha-nvim',
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+      'nvim-lua/plenary.nvim',
+    },
+    config = function()
+      require('alpha').setup(require('alpha.themes.theta').config)
+    end,
+  },
+}
