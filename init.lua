b390e34586c2bd08037a58e38515876d87588636
the_birchmen <dustymorse@gmail.com>
2024-05-20 20:50:38 -0400

    general config updates

--- init.lua
+++ init.lua
@@ -74,9 +74,6 @@ require('lazy').setup {
 
   'ap/vim-css-color',
 
-  -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim', opts = {} },
-
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
