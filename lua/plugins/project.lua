5c1f029f27dc6124233a739317be40014d96e599
the_birchmen <dustymorse@gmail.com>
2024-06-28 19:01:34 -0400

    project.nvim

---
+++ lua/plugins/project.lua
@@ -0,0 +1,14 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  'ahmedkhalf/project.nvim',
+  config = function()
+    require('project_nvim').setup {
+      patterns = { '.git', '_darcs', '.hg', '.bzr', '.svn', 'Makefile', 'package.json', '.obsidian' }, -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  end,
+}
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -84,6 +84,7 @@ return {
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = 'Find existing [B]uffers' })
+      vim.keymap.set('n', '<leader>sp', ':Telescope projects<CR>', { desc = '[S]earch [P]rojects' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
