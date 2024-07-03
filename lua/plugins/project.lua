8f32f543db31931e9bc9d28997e43a3a76cf1d04
the_birchmen <dustymorse@gmail.com>
2024-07-02 20:58:07 -0400

    updated README.md

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -6,7 +6,8 @@ return {
   'ahmedkhalf/project.nvim',
   config = function()
     require('project_nvim').setup {
-      patterns = { '.git', '_darcs', '.hg', '.bzr', '.svn', 'Makefile', 'package.json', '.obsidian' }, -- your configuration comes here
+      patterns = { '.git', '_darcs', '.hg', '.bzr', '.svn', 'Makefile', 'package.json', '.obsidian' },
+      -- your configuration comes here
       -- or leave it empty to use the default settings
       -- refer to the configuration section below
     }
