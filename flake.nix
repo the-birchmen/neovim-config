bd16941be22fdc1b71f92af75068d7c2f4641148
Dustin Morse <dustymorse@gmail.com>
2026-06-20 10:47:27 -0400

    addind initial flake

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Neovim Flake (Stable)";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+  in {
+    packages.${system} = {
+      default = pkgs.neovim;
+    };
+  };
+}
